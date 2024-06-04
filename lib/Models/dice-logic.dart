5e2192224331f7eef8ac090ecc876e7f808a8c4d
Jawwad Abbasi <jawwadabbasi990@gmail.com>
2024-06-03 22:36:15 -0400

    Complete logic done.

--- lib/Models/dice-logic.dart
+++ lib/Models/dice-logic.dart
@@ -1,14 +1,16 @@
+import 'dart:async';
 import 'dart:math';
 import 'dart:io';
 
 import 'package:dice_game/Constants/constants.dart';
 
 class DiceLogic {
-  int _round = 10;
+  int _round = 1;
   int _diceNumber = 0;
-  int playerPoints = 0;
-  int rivalpoints = 0;
-  var random = Random().nextInt(6) + 1;
+  int _playerPoints = 0;
+  int _rivalpoints = 0;
+  int _random = 0;
+  // var _random = Random();
 
   void _displayHeader() {
     for (var element in header) {
@@ -22,8 +24,54 @@ class DiceLogic {
     print(helpMessage);
   }
 
-  void diceLogicFunc() {
+  Future<void> diceLogicFunc() async {
     _displayHeader();
     _displayHelpMessage();
+    stdout.write("Press any key to continue... ");
+    // stdin.echoMode = false; // Optional: Disable echoing of pressed key
+    int? key = await stdin.readByteSync();
+
+    while (_round <= 10) {
+      print('');
+
+      print('-------------- Round: $_round --------------');
+      _random = Random().nextInt(6) + 1;
+      print('Computer rolled $_random');
+      int computerRandom = _random;
+      print('');
+
+      print('Press any key to roll your dice');
+      int? key = await stdin.readByteSync();
+      _random = Random().nextInt(6) + 1;
+      print('Player rolled $_random');
+      int playerRandom = _random;
+      print('');
+
+      if (playerRandom < computerRandom) {
+        _rivalpoints++;
+        print('Rival score: $_rivalpoints and Player score: $_playerPoints');
+      } else if (playerRandom > computerRandom) {
+        _playerPoints++;
+        print('Rival score: $_rivalpoints and Player score: $_playerPoints');
+      } else {
+        print(
+            'Round draw! No points increase. Rival score: $_rivalpoints and Player score: $_playerPoints');
+      }
+      // round incrementor
+      _round++;
+    }
+    print('');
+    winnerMessage(_playerPoints, _rivalpoints);
+    print('');
+  }
+
+  String winnerMessage(int _playerPoints, int _rivalpoints) {
+    if (_playerPoints > _rivalpoints) {
+      return 'Player wins!';
+    } else if (_playerPoints < _rivalpoints) {
+      return 'Computer wins!';
+    } else {
+      return 'Game Draw';
+    }
   }
 }
